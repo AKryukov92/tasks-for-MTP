1bbf32ece958ec39102b26905395ed8a2d9437e6
Alexander Kryukov <akryukov@luxoft.com>
2016-03-25 16:10:26 +0600

    Теперь в курсе используется та база, к которой студенты писали запросы на курсе по БД.

--- BabylonARM/BabylonARM/ProductCatalog.Designer.cs
+++ BabylonARM/BabylonARM/ProductCatalog.Designer.cs
@@ -40,9 +40,9 @@
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnSave = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.productCard = new System.Windows.Forms.GroupBox();
             this.btnRefresh = new System.Windows.Forms.Button();
-            this.groupBox1.SuspendLayout();
+            this.productCard.SuspendLayout();
             this.SuspendLayout();
             // 
             // listProducts
@@ -50,7 +50,7 @@
             this.listProducts.FormattingEnabled = true;
             this.listProducts.Location = new System.Drawing.Point(12, 12);
             this.listProducts.Name = "listProducts";
-            this.listProducts.Size = new System.Drawing.Size(120, 134);
+            this.listProducts.Size = new System.Drawing.Size(156, 134);
             this.listProducts.TabIndex = 0;
             this.listProducts.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
@@ -65,14 +65,20 @@
             // 
             // txtName
             // 
+            this.txtName.AutoCompleteCustomSource.AddRange(new string[] {
+            "abc",
+            "abcd",
+            "abcde"});
+            this.txtName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
             this.txtName.Location = new System.Drawing.Point(131, 19);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(100, 20);
-            this.txtName.TabIndex = 2;
+            this.txtName.TabIndex = 1;
             // 
             // txtCost
             // 
-            this.txtCost.Location = new System.Drawing.Point(131, 98);
+            this.txtCost.Location = new System.Drawing.Point(131, 72);
             this.txtCost.Name = "txtCost";
             this.txtCost.Size = new System.Drawing.Size(100, 20);
             this.txtCost.TabIndex = 3;
@@ -81,24 +87,25 @@
             // 
             this.cmbUnit.FormattingEnabled = true;
             this.cmbUnit.Items.AddRange(new object[] {
+            "",
             "штука",
             "пара"});
             this.cmbUnit.Location = new System.Drawing.Point(131, 45);
             this.cmbUnit.Name = "cmbUnit";
             this.cmbUnit.Size = new System.Drawing.Size(100, 21);
-            this.cmbUnit.TabIndex = 5;
+            this.cmbUnit.TabIndex = 2;
             // 
             // txtQuantity
             // 
-            this.txtQuantity.Location = new System.Drawing.Point(131, 124);
+            this.txtQuantity.Location = new System.Drawing.Point(131, 98);
             this.txtQuantity.Name = "txtQuantity";
             this.txtQuantity.Size = new System.Drawing.Size(100, 20);
-            this.txtQuantity.TabIndex = 6;
+            this.txtQuantity.TabIndex = 4;
             // 
             // lblCost
             // 
             this.lblCost.AutoSize = true;
-            this.lblCost.Location = new System.Drawing.Point(6, 101);
+            this.lblCost.Location = new System.Drawing.Point(6, 75);
             this.lblCost.Name = "lblCost";
             this.lblCost.Size = new System.Drawing.Size(92, 13);
             this.lblCost.TabIndex = 7;
@@ -116,7 +123,7 @@
             // lblQuantity
             // 
             this.lblQuantity.AutoSize = true;
-            this.lblQuantity.Location = new System.Drawing.Point(6, 127);
+            this.lblQuantity.Location = new System.Drawing.Point(6, 101);
             this.lblQuantity.Name = "lblQuantity";
             this.lblQuantity.Size = new System.Drawing.Size(66, 13);
             this.lblQuantity.TabIndex = 9;
@@ -124,20 +131,20 @@
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(156, 170);
+            this.btnDelete.Location = new System.Drawing.Point(156, 140);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(75, 23);
-            this.btnDelete.TabIndex = 10;
+            this.btnDelete.TabIndex = 6;
             this.btnDelete.Text = "Удалить";
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(12, 153);
+            this.btnAdd.Location = new System.Drawing.Point(93, 152);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(75, 23);
-            this.btnAdd.TabIndex = 11;
+            this.btnAdd.TabIndex = 8;
             this.btnAdd.Text = "Добавить";
             this.btnAdd.UseVisualStyleBackColor = true;
             this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
@@ -145,39 +152,39 @@
             // btnSave
             // 
             this.btnSave.Enabled = false;
-            this.btnSave.Location = new System.Drawing.Point(75, 170);
+            this.btnSave.Location = new System.Drawing.Point(75, 140);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(75, 23);
-            this.btnSave.TabIndex = 12;
+            this.btnSave.TabIndex = 5;
             this.btnSave.Text = "Сохранить";
             this.btnSave.UseVisualStyleBackColor = true;
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
-            // groupBox1
+            // productCard
             // 
-            this.groupBox1.Controls.Add(this.lblName);
-            this.groupBox1.Controls.Add(this.btnSave);
-            this.groupBox1.Controls.Add(this.txtName);
-            this.groupBox1.Controls.Add(this.txtCost);
-            this.groupBox1.Controls.Add(this.btnDelete);
-            this.groupBox1.Controls.Add(this.cmbUnit);
-            this.groupBox1.Controls.Add(this.lblQuantity);
-            this.groupBox1.Controls.Add(this.txtQuantity);
-            this.groupBox1.Controls.Add(this.lblUnit);
-            this.groupBox1.Controls.Add(this.lblCost);
-            this.groupBox1.Location = new System.Drawing.Point(154, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(237, 199);
-            this.groupBox1.TabIndex = 13;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Карточка товара";
+            this.productCard.Controls.Add(this.lblName);
+            this.productCard.Controls.Add(this.btnSave);
+            this.productCard.Controls.Add(this.txtName);
+            this.productCard.Controls.Add(this.txtCost);
+            this.productCard.Controls.Add(this.btnDelete);
+            this.productCard.Controls.Add(this.cmbUnit);
+            this.productCard.Controls.Add(this.lblQuantity);
+            this.productCard.Controls.Add(this.txtQuantity);
+            this.productCard.Controls.Add(this.lblUnit);
+            this.productCard.Controls.Add(this.lblCost);
+            this.productCard.Location = new System.Drawing.Point(174, 12);
+            this.productCard.Name = "productCard";
+            this.productCard.Size = new System.Drawing.Size(237, 171);
+            this.productCard.TabIndex = 1;
+            this.productCard.TabStop = false;
+            this.productCard.Text = "Карточка товара";
             // 
             // btnRefresh
             // 
-            this.btnRefresh.Location = new System.Drawing.Point(12, 182);
+            this.btnRefresh.Location = new System.Drawing.Point(12, 152);
             this.btnRefresh.Name = "btnRefresh";
             this.btnRefresh.Size = new System.Drawing.Size(75, 23);
-            this.btnRefresh.TabIndex = 14;
+            this.btnRefresh.TabIndex = 7;
             this.btnRefresh.Text = "Обновить";
             this.btnRefresh.UseVisualStyleBackColor = true;
             this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
@@ -186,15 +193,16 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 219);
+            this.ClientSize = new System.Drawing.Size(414, 188);
             this.Controls.Add(this.btnRefresh);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.productCard);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.listProducts);
             this.Name = "ProductCatalog";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Справочник товаров";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.productCard.ResumeLayout(false);
+            this.productCard.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -213,7 +221,7 @@
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnSave;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox productCard;
         private System.Windows.Forms.Button btnRefresh;
     }
 }
